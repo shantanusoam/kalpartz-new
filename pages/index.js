84304144b3ff06641468dedad04d71f0f1662a2a
Kd <umarkhurshid3@gmail.com>
2022-11-03 03:11:16 +0530

    update in index js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Home = () => (
 
     <AboutUs />
     <WhatMakeUsSpecial />
-    <NewDeerGuard></NewDeerGuard>
+    <NewDeerGuard />
     <BrandsComponent />
     {/* <FeaturedProducts /> */}
     {/* <RoadSideAssistant /> */}
